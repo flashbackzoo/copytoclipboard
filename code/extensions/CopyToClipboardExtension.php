b2d5da7ad9301b13ca95d591a4bb379bf5452025
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:19:46 +1300

    Converted to PSR-2

--- code/extensions/CopyToClipboardExtension.php
+++ code/extensions/CopyToClipboardExtension.php
@@ -1,9 +1,11 @@
 <?php
 
-class CopyToClipboardExtension extends DataExtension {
-	public function init() {
-		Requirements::css('copytoclipboard/css/copytoclipboard.css');
-		Requirements::javascript('copytoclipboard/javascript/ZeroClipboard.min.js');
-		Requirements::javascript('copytoclipboard/javascript/main.js');
-	}
+class CopyToClipboardExtension extends DataExtension
+{
+    public function init()
+    {
+        Requirements::css('copytoclipboard/css/copytoclipboard.css');
+        Requirements::javascript('copytoclipboard/javascript/ZeroClipboard.min.js');
+        Requirements::javascript('copytoclipboard/javascript/main.js');
+    }
 }
